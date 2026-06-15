a06af2e5aeb65d68ab6dda22c0d9d2a0e732dd01
cse25bcsh96-hue <cse.25bcsh96@silicon.ac.in>
2026-06-15 23:50:17 +0530

    Update String_Processor.java

--- String_Processor.java
+++ String_Processor.java
@@ -1,19 +1,31 @@
-/*
-String Processor with Exception Safety
+package JAVA_DAY_10;
+import java.util.Scanner;
+import java.lang.StringIndexOutOfBoundsException;
+public class STRING_PROCESSOR {
 
-Create a program that:
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the string:");
+		String string = sc.nextLine();
+		try {
+			System.out.println("Enter the index bounds for the substring:");
+			System.out.print("Start: ");
+			int a = sc.nextInt();
+			System.out.print("End: ");
+			int b = sc.nextInt();
+			
+			System.out.println("Substring: "+string.substring(a,b));
+			
+			System.out.println("Enter the character to access of string");
+			System.out.print("Enter index: ");
+			a = sc.nextInt();
+			System.out.println("Character at index "+a+" is: "+string.charAt(a));
+		}catch(StringIndexOutOfBoundsException e)
+		{
+			System.out.println("Invalid index handled safely");
+		}
+		sc.close();
+	}
 
-Takes a string input
-
-Performs:
-
-substring operation
-charAt operation
-Requirements:
-
-Handle:
-
-StringIndexOutOfBoundsException
-Example:
-Invalid index handled safely
-*/
+}
