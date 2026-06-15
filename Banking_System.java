a96c77140c11314e9ab42d05d3dce9c11e754447
cse25bcsh96-hue <cse.25bcsh96@silicon.ac.in>
2026-06-15 23:49:40 +0530

    Add withdrawal feature with error handling
    Implemented withdrawal functionality with exception handling for insufficient balance and invalid input.

--- Banking_System.java
+++ Banking_System.java
@@ -1,13 +1,37 @@
-/*
-Banking System (Real-world Simulation)
+package JAVA_DAY_10;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class BANKING_SYSTEM {
 
-Create a program that:
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 
-Takes account balance
-Takes withdrawal amount
-Rules:
-If withdrawal > balance → throw and handle exception
-If invalid input → handle exception
-Output:
-Withdrawal successful OR Insufficient balance
-*/
+        try {
+            System.out.print("Enter account balance: ");
+            double balance = sc.nextDouble();
+
+            System.out.print("Enter withdrawal amount: ");
+            double withdraw = sc.nextDouble();
+
+            if (withdraw > balance) {
+                throw new Exception("Insufficient balance");
+            }
+
+            balance -= withdraw;
+
+            System.out.println("Withdrawal successful");
+            System.out.println("Remaining balance: " + balance);
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Invalid input! Please enter numeric values.");
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        sc.close();
+	}
+
+}
